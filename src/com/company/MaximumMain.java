63478ac4e6664ac9d8c400bee0388962c911e967
Srinath S <srinath@Srinaths-MacBook-Air.local>
2021-12-19 11:43:11 +0530

    First commit

---
+++ src/com/company/MaximumMain.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MaximumMain {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Maximum obj = new Maximum();
+
+        System.out.print(" Please enter the first value: ");
+        String first = scan.next();
+
+        System.out.print(" Please enter the second value: ");
+        String second = scan.next();
+
+        System.out.print(" Please enter the third value: ");
+        String third = scan.next();
+
+        obj.findMaxString(first, second, third);
+
+        scan.close();
+    }
+
+}
+
